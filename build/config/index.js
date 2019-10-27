20eb7b80ed9f86b937abab98c05468c1130339e7
云墨 <lg208143@alibaba-inc.com>
2019-10-27 17:13:31 +0800

    feat: delete build

--- build/config/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var config = {
-    // 需要校验的文件后缀, 支持正则、函数类型
-    checkFileExtname: ['js', 'jsx', 'tsx']
-};
-exports.default = config;
